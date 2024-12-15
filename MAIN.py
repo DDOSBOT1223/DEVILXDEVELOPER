42db7e556a9852bd19bed01b139e04cdb8093907
DDOSBOT1223 <ownerdevil2.4@gmail.com>
2024-12-15 07:41:15 +0530

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -16,7 +16,7 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = ''7837130437:AAFoD7W9q3yAs9zQBrHN8VYRVPlAziurwZE
+TOKEN = '7837130437:AAFoD7W9q3yAs9zQBrHN8VYRVPlAziurwZE'
 MONGO_URI = 'mongodb+srv://Bishal:Bishal@bishal.dffybpx.mongodb.net/?retryWrites=true&w=majority&appName=Bishal'
 FORWARD_CHANNEL_ID = -1002275893076
 CHANNEL_ID = -1002275893076
